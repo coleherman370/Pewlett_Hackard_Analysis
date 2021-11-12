1d627e15147071da167f48d7f3508724b82554f0
Cole Herman <cole.herman370@outlook.com>
2021-11-11 18:29:22 -0600

    Updating schema.sql

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,6 +1,6 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
+     dept_no VARCHAR(40) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
@@ -21,8 +21,6 @@ CREATE TABLE dept_manager (
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -39,17 +37,13 @@ CREATE TABLE dept_emp (
   dept_no varchar NOT NULL,
   emp_no INT NOT NULL,
   from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no, emp_no)
+  to_date DATE NOT NULL
 );
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title int NOT NULL,
+  title varchar NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
